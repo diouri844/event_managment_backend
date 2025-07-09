3f266762eabc0593bb64d99463b0eda98aa12c3e
Chopen <salahiddine.diouri@usmba.ac.ma>
2025-07-09 22:31:45 +0200

    Remove JWT requirement from user routes and update error messages for better clarity

--- src/users/routes.py
+++ src/users/routes.py
@@ -8,7 +8,7 @@ from flask_jwt_extended import jwt_required, get_jwt_identity
 users_bp = Blueprint("users", __name__)
 
 @users_bp.route("/users", methods=["GET"])
-@jwt_required()
+# @jwt_required()
 def get_users():
     """
     Get all users with pagination support.
@@ -36,11 +36,11 @@ def get_users():
         }
         return jsonify(res_object), 200
     except Exception as e:
-        return jsonify({"error": str(e)}), 500
+        return jsonify({"error": "Server failed to fetch user list "}), 500
     
 
 @users_bp.route("/users/<int:user_id>", methods=["GET"])
-@jwt_required()
+# @jwt_required()
 def get_user(user_id):
     """
     Get a user by ID.
@@ -60,12 +60,12 @@ def get_user(user_id):
         }
         return jsonify(res_object), 200
     except Exception as e:
-        return jsonify({"error": str(e)}), 500
+        return jsonify({"error": "Server failed to fetch user by id"}), 500
 
 
 
 @users_bp.route("/users", methods=["POST"])
-@jwt_required()
+# @jwt_required()
 def create_user():
     """
     Create a new user.
@@ -94,11 +94,11 @@ def create_user():
         return jsonify(res_object), 201
     except Exception as e:
         db.session.rollback()  # Rollback the session in case of error
-        return jsonify({"error": str(e)}), 500
+        return jsonify({"error":"Server failed to create new user "}), 500
     
 
 @users_bp.route("/users/<int:user_id>", methods=["PUT"])
-@jwt_required()
+# @jwt_required()
 def update_user(user_id):
     """
     Update an existing user by ID.
@@ -127,6 +127,6 @@ def update_user(user_id):
         return jsonify(res_object), 200
     except Exception as e:
         db.session.rollback()
-        return jsonify({"error": str(e)}), 500
+        return jsonify({"error": "Server failed to update user by id "}), 500
 
 
