80533460bf2ccdff2a4675b80382ef7daebd02f0
Chopen <salahiddine.diouri@usmba.ac.ma>
2025-07-16 15:12:48 +0200

    Enable SQLALCHEMY_TRACK_MODIFICATIONS in config.py

--- src/config.py
+++ src/config.py
@@ -5,5 +5,5 @@ class Config:
         "DATABASE_URL",
         "postgresql://flaskuser:flaskpass@localhost:5432/flaskdb"
     )
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_TRACK_MODIFICATIONS = True
     JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY", "super-secret-key")
