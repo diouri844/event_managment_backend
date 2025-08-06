9efca83e9ca882e1de576d7d101fce8b09724087
Chopen <salahiddine.diouri@usmba.ac.ma>
2025-08-06 15:48:11 +0200

    move to deno hono and drizzl stack

--- src/config.py
+++
@@ -1,9 +0,0 @@
-import os
-
-class Config:
-    SQLALCHEMY_DATABASE_URI = os.getenv(
-        "DATABASE_URL",
-        "postgresql://flaskuser:flaskpass@localhost:5432/flaskdb"
-    )
-    SQLALCHEMY_TRACK_MODIFICATIONS = True
-    JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY", "super-secret-key")
--- src/utils/show_routes.py
+++
@@ -1,8 +0,0 @@
-# utils/show_routes.py
-
-def show_routes(app):
-    with app.app_context():
-        for rule in app.url_map.iter_rules():
-            methods = ','.join(sorted(rule.methods - {'HEAD', 'OPTIONS'}))
-            print(f"{rule.endpoint:30s} {methods:10s} {rule.rule}")
-            
