8444fde59d22f98fc39ab87e437bcdfae7d1a930
Chopen <salahiddine.diouri@usmba.ac.ma>
2025-08-06 22:43:34 +0200

    Fix import statement for dotenv in drizzle configuration

--- src/config/drizzle.config.ts
+++ src/config/drizzle.config.ts
@@ -1,6 +1,6 @@
 // drizzle.config.ts
 import { defineConfig } from 'drizzle-kit';
-
+import 'dotenv/config';
 export default defineConfig({
     schema: './src/db/schema.ts', // adjust to your actual schema path
     out: '../drizzle',
