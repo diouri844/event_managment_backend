aac078c4bc9e5fdcf46af9bb6fae5a64fc3bdfee
Chopen <salahiddine.diouri@usmba.ac.ma>
2025-08-11 22:18:17 +0200

    Refactor user route to use user router instead of user controller

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import { Hono } from 'hono'
-import userController from './controllers/user.controller';
+import userRouter from './routers/user.router';
 const app = new Hono()
 
-app.route('/user', userController)
+app.route('/user', userRouter)
 export default app
