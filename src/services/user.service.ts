74341dd71e88acd1ae500b963c751c7e8d9646e0
Chopen <salahiddine.diouri@usmba.ac.ma>
2025-08-11 22:19:11 +0200

    Remove unused user service functions and export EventServiceProvider class

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -1,15 +1,3 @@
-import { Context } from "hono";
 
-export function ping(c: Context) {
-    return c.json({
-        message: 'Hello, World!',
-        timestamp: new Date().toISOString(),
-        status: 'success',
-    });
-}
 
-export async function listUsers(c: Context) { };
-
-export async function loginUser(c: Context) { };
-
-export async function registerUser(c: Context) { };
+export default class EventServiceProvider { };
