5d7b312654ba7eddedbdd9507fae0377308a29ab
Chopen <salahiddine.diouri@usmba.ac.ma>
2025-08-12 22:55:04 +0200

    add event service crud operation

--- src/services/event.service.ts
+++ src/services/event.service.ts
@@ -1,10 +1,61 @@
 
+// import the db instance 
+import { CreateVentDto, IEvent, UpdateEventDto } from '../types/event.type';
+import { db } from '../utils/instance.db';
+import { Events } from '../Db/schema';
+import { eq } from 'drizzle-orm';
 
 
 export default class EventServiceProvider {
-    async createEvent(eventData: any): Promise<any> { }
-    async getEventById(eventId: number): Promise<any> { }
-    async getAllEvents(): Promise<any> { }
-    async updateEvent(eventId: number, updatedData: any): Promise<any> { }
-    async deleteEvent(eventId: number): Promise<any> { }
+    async createEvent(eventData: CreateVentDto): Promise<typeof Events.$inferInsert | null> {
+        try {
+            const result = await db.insert(Events).values(eventData).returning();
+            return result[0];
+        } catch (error) {
+            console.error('Service:Error creating event:', error);
+            return null;
+        }
+    };
+    async getEventById(eventId: number): Promise<typeof Events.$inferSelect | null> {
+        try {
+            const result = await db.select().from(Events)
+                .where(eq(Events.id, eventId)).limit(1);
+            return result[0];
+        } catch (error) {
+            console.error('Service:Error fetching event by id :', error);
+            return null;
+        }
+    }
+    async getAllEvents(paginationOption: any): Promise<typeof Events.$inferSelect[] | null> {
+        try {
+            const { limit, offset } = paginationOption;
+            const result = await db.select().from(Events).limit(limit).offset(offset);
+            return result;
+        } catch (error) {
+            console.error('Service:Error fetching event list  :', error);
+            return null
+        }
+    }
+    async updateEvent(
+        eventId: number,
+        updatedData: UpdateEventDto): Promise<
+            typeof Events.$inferSelect | null
+        > {
+        try {
+            const result = await db.update(Events).set(updatedData).where(eq(Events.id, eventId)).returning();
+            return result[0];
+        } catch (error) {
+            console.error('Service:Error updating  event by id   :', error);
+            return null
+        }
+    }
+    async deleteEvent(eventId: number): Promise<typeof Events.$inferSelect | null> {
+        try {
+            const result = await db.delete(Events).where(eq(Events.id, eventId)).returning();
+            return result[0];
+        } catch (error) {
+            console.error('Service:Error deleting  event by id   :', error);
+            return null
+        }
+    }
 };
