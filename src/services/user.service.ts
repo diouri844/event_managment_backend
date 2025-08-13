0cc942bf48266b288ee9c6e477535a49cdc0fc2f
Chopen <salahiddine.diouri@usmba.ac.ma>
2025-08-13 22:19:59 +0200

    setup user service provider

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -1,3 +1,45 @@
+import { Users } from "../Db/schema";
+import { UserSelect } from "../types/user.type";
+import { db } from "../utils/instance.db";
+import { eq } from "drizzle-orm";
 
 
-export default class EventServiceProvider { };
+
+
+
+export default class UserServiceProvider {
+    async findUserById(userId: number): Promise<UserSelect | null> {
+        try {
+            const targetUser = await db
+                .select({
+                    id: Users.id,
+                    name: Users.username, // add other fields you want to select
+                    email: Users.email,
+                    role: Users.role,
+                })
+                .from(Users)
+                .where(eq(Users.id, userId))
+                .limit(1);
+            return targetUser[0];
+        } catch (error) {
+            console.log("Service:Error finding user by ID:", error);
+            return null;
+        }
+    };
+
+    async fetchUserbyEmail(email: string): Promise<UserSelect | null> {
+        try {
+            const targetUser = await db.select({
+                id: Users.id,
+                name: Users.username,
+                email: Users.email,
+                role: Users.role,
+            }).from(Users).where(eq(Users.email, email)).limit(1);
+            return targetUser[0];
+        } catch (error) {
+            console.log("Service:Error finding user by Email:", error);
+            return null;
+        }
+    };
+
+};
