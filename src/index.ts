06e405795b0aacc3e3f3f3b5b68b0b2336d1911d
Chopen <salahiddine.diouri@usmba.ac.ma>
2025-08-13 22:02:46 +0200

    update app main entry point

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import { Hono } from 'hono'
 import userRouter from './routers/user.router';
+import eventRouter from './routers/event.router';
 const app = new Hono()
 
-app.route('/user', userRouter)
+app.route('/user', userRouter);
+app.route('/event', eventRouter);
 export default app
